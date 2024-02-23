cc0199a622f3fa3e9f27f5adaa3a1bd46abebd5b
jenny <dooly3392@gmail.com>
2024-02-23 23:02:25 +0900

    prop이 바뀌지 않는 컴포넌트를 렌더링 하지 않도록 함

---
+++ src/components/DragabbleCard.tsx
@@ -0,0 +1,34 @@
+import React from "react";
+import { Draggable } from "react-beautiful-dnd";
+import styled from "styled-components";
+
+const Card = styled.div`
+  border-radius: 5px;
+  margin-bottom: 5px;
+  padding: 10px 10px;
+  background-color: ${(props) => props.theme.cardColor};
+`;
+
+interface IDragabbleCardProps {
+  toDo: string;
+  index: number;
+}
+
+function DragabbleCard({ toDo, index }: IDragabbleCardProps) {
+  return (
+    <Draggable key={toDo} draggableId={toDo} index={index}>
+      {(magic) => (
+        <Card
+          ref={magic.innerRef}
+          {...magic.draggableProps}
+          {...magic.dragHandleProps}
+        >
+          {toDo}
+        </Card>
+      )}
+    </Draggable>
+  );
+}
+
+//react에게 prop이 변하지 않았다면 DraggableCard를 다시 렌더링하지 말라고 하는것
+export default React.memo(DragabbleCard);
